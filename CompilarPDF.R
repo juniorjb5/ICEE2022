b77cda7b9e786542db493bb010a7f69c8bb0737f
Junior Joaqui B <juniorjb5@hotmail.com>
2022-05-26 17:37:51 -0500

    Slides_13

--- CompilarPDF.R
+++ CompilarPDF.R
@@ -3,7 +3,7 @@
 
 
 git add .
-git commit -m "Slides_11"
+git commit -m "Slides_12"
 git push origin main
 
 
@@ -18,8 +18,8 @@ remotes::install_github("jhelvy/xaringanBuilder", dependencies = TRUE)
 library(xaringanBuilder)
 
 
-build_pdf("Presentation_ICEE_2022.Rmd", complex_slides = TRUE, partial_slides = TRUE)
-build_pdf("Presentation_ICEE_2022.html", complex_slides = TRUE, partial_slides = TRUE)
+#build_pdf("Presentation_ICEE_2022.Rmd", complex_slides = TRUE, partial_slides = TRUE)
+#build_pdf("Presentation_ICEE_2022.html", complex_slides = TRUE, partial_slides = TRUE)
 
 
 build_pdf("PDF_ICEE_2022.html", complex_slides = TRUE, partial_slides = TRUE)
